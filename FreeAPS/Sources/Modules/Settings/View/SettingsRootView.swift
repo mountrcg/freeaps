2461bcb77170438f791fb10e05ea8f036db899ff
Robert <comps@diprobe.com>
2022-09-12 08:16:20 +0200

    add garmin device management

--- FreeAPS/Sources/Modules/Settings/View/SettingsRootView.swift
+++ FreeAPS/Sources/Modules/Settings/View/SettingsRootView.swift
@@ -17,6 +17,7 @@ extension Settings {
                 Section(header: Text("Devices")) {
                     Text("Pump").navigationLink(to: .pumpConfig, from: self)
                     Text("CGM").navigationLink(to: .cgm, from: self)
+                    Text("Garmin watch").navigationLink(to: .garmin, from: self)
                 }
 
                 Section(header: Text("Services")) {
