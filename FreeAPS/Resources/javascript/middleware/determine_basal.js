c5ff60ccc3d46a1f3246a6a48e1d2b5901077aca
Robert <comps@diprobe.com>
2022-12-19 16:51:56 +0100

    middleware date.hour fix in sample, but sitched off

--- FreeAPS/Resources/javascript/middleware/determine_basal.js
+++ FreeAPS/Resources/javascript/middleware/determine_basal.js
@@ -1,5 +1,24 @@
-function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory, preferences, basalProfile) {
-    // modify anything
-    // return any reason what has changed.
-    return "Nothing changed";
+function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pump_history, preferences, basalProfile, tdd, tdd_daily, tdd_avg) {
+
+    //disable SMBs at night
+    var nightlySMBswitch = 0;
+    var reasonSMBswitch = "nothing done";
+    
+    const d = new Date();
+    let currentHour = d.getHours();
+
+    // Turn SMBs off at night
+    if (nightlySMBswitch == 1) {
+        if (currentHour >= 0 && currentHour <= 7) {
+            profile.enableSMB_always = false;
+            profile.enableUAM = false;
+            reasonSMBswitch = "SMBs disabled due to nighttime " + currentHour + ":00";
+        } else {
+            profile.enableSMB_always = true;
+            profile.enableUAM = true;
+            reasonSMBswitch = "SMBs enabled due to daytime " + currentHour + ":00";
+        }
+    }
+    // end function and supply log output
+    return reasonSMBswitch
 }
--- middleware.js
+++ middleware.js
@@ -1,39 +1,36 @@
 
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory, preferences, basalProfile) {
     //enable SMB at night with High Targets and disable after 6am
-     var nightlySMBHT = 1 ;
+     var nightlySMBHT = 0 ;
+     var reasonSMB = "";
+     var SMBoff = 0;
+     var reasonSMBswitch = "";
+
+    const d = new Date();
+    let currentHour = d.getHours();
 
     // Turn SMB`s with High targets on during nights
-    function hourInt(base_timeStamp) {
-        function addZero(i) {
-            if (i < 10) {i = "0" + i}
-            return i;
-        }
-        let hour = addZero(base_timeStamp.getHours());
-        return hour;
-    }
-
     if (nightlySMBHT == 1) {
-        if (hourInt >= 0 && hours <= 6) {
+        if (currentHour >= 0 && currentHour <= 6) {
             profile.allowSMB_with_high_temptarget = true;
-            reasonSMB = "SMB with HighTarget enabled due to nighttime. "
+            reasonSMB = "SMB with HighTarget enabled due to nighttime " + currentHour + ":00."
             }else{
                 profile.allowSMB_with_high_temptarget = false
-                reasonSMB = "SMB with HighTarget disabled due to daytime. "
+                reasonSMB = "SMB with HighTarget disabled due to daytime " + currentHour + ":00."
             }
         }
 
-    // Turn off DynamicISF and AutoISF when using a temp target >= 110 and if an exercise setting is enabled.
-    const autoswitchoff = preferences.switchSportXPM;
-    const currentMinTarget = profile.min_bg;
-    var reasonSports  = "";
-    if ((profile.high_temptarget_raises_sensitivity == true || profile.exercise_mode == true) && autoswitchoff) {
-        exerciseSetting = true;
+    // Turn SMBs off at night
+    if (SMBoff=1) {
+        if (currentHour >= 0 && currentHour <= 7) {
+            profile.enableSMB_always = false;
+            profile.enableUAM = false;
+            reasonSMBswitch = "SMBs disabled due to nighttime " + currentHour + ":00."
+        }else{
+            profile.enableSMB_always = true;
+            profile.enableUAM = true;
+            reasonSMBswitch = "SMBs enabled due to daytime " + currentHour + ":00."
+        }
     }
-    if (profile.temptargetSet && profile.min_bg > 109 && exerciseSetting == true) {
-        profile.use_autoisf = false;
-        reasonSports = "autoISF off due to Exercise Target. Current min target: " + currentMinTarget + "! ";
-    }
-
-    return `${reasonSMB} ${reasonSports}`
+    return `${reasonSMB} ${reasonSMBswitch}`
 }
